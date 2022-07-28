de1f4d58c6be4c8b0871a583f454620223344d21
ck1e <ck1e.sty@gmail.com>
2022-07-28 20:17:46 +0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,13 @@
+# Autoclicker for hh.ru
+
+<hr>
+
+Using this script, you can automatically send responses to vacancies according to the specified filters
+
+Before starting you need to specify:
+* URL
+* LOGIN 
+* PASSWORD 
+* KW - Search keywords you are interested in
+* EXPERIENCE - Work experience you are interested in
+* REQUEST_PARAMS - Other search filters (matches query GET parameters)
---
+++ main.py
@@ -0,0 +1,107 @@
+from urllib import parse
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.common.by import By
+from selenium.common.exceptions import NoSuchElementException
+
+
+URL = 'https://.hh.ru'
+LOGIN = '***'
+PASSWORD = '***'
+
+# Search keywords
+KW = ('Wordpress', 'Python', 'Web')
+
+# Desired work experience
+EXPERIENCE = ('between1And3', 'noExperience', 'doesNotMatter')
+
+# All search filters
+REQUEST_PARAMS = {
+    'text': '',
+    'experience': '',
+    'search_period': '3',
+    'schedule': 'remote',
+    'items_on_page': '100'
+}
+
+# All ID of suitable vacancies
+_vacancy_id = []
+
+# ID of vacancies for which it was not possible to send a response due to the requirement of a preliminary test
+no_response_vacancy_id = []
+
+
+def response_vacancy(driver) -> None:
+    """ Collects all job IDs and then applies for them
+
+    :param driver: webdriver Chrome
+    :return: None
+    """
+    try:
+        # Gets all ids for the current request
+        links = driver.find_elements(By.XPATH, '//a[contains(@href, "vacancy_response?vacancyId=")]')
+        for link in links:
+            _vacancy_id.append(
+                parse.parse_qs(parse.urlparse(link.get_attribute('href')).query)['vacancyId'][0]
+            )
+
+        # Go to the next page and repeat the same
+        driver.find_element(By.XPATH, '//a[@data-qa="pager-next"]').click()
+        return response_vacancy(driver)
+
+    # If there is no next page, then we respond to vacancies
+    except NoSuchElementException:
+        for vacancy_id in _vacancy_id:
+            driver.get(f'{URL}/applicant/vacancy_response?vacancyId={vacancy_id}')
+
+            # If you need to confirm the response
+            try:
+                driver.find_element(By.XPATH, '//button[@data-qa="relocation-warning-confirm"]').click()
+            except NoSuchElementException:
+                pass
+
+            # Check if the response succeeded
+            try:
+                driver.find_element(By.XPATH, f'//a[@data-qa="vacancy-response-link-view-topic"][contains(@href, "topicId={vacancy_id}")]')
+            except NoSuchElementException:
+                no_response_vacancy_id.append(vacancy_id)
+
+    return None
+
+
+def main():
+    # Opening the browser
+    driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
+    driver.maximize_window()
+
+    try:
+        # Authorization
+        driver.get(f'{URL}/account/login')
+        driver.find_element(By.XPATH, '//button[@data-qa="expand-login-by-password"]').click()
+        driver.implicitly_wait(10)
+        driver.find_element(By.XPATH, '//input[@name="username"][@type="text"]').send_keys(LOGIN)
+        driver.find_element(By.XPATH, '//input[@type="password"]').send_keys(PASSWORD)
+        driver.find_element(By.XPATH, '//button[@data-qa="account-login-submit"]').click()
+
+        driver.switch_to.new_window('tab')
+        driver.get(URL)
+
+        # Search for vacancies by relevant keywords
+        for kw in KW:
+            for exp in EXPERIENCE:
+                REQUEST_PARAMS['text'] = kw
+                REQUEST_PARAMS['experience'] = exp
+
+                driver.get(f'{URL}/search/vacancy?{parse.urlencode(REQUEST_PARAMS)}')
+
+                response_vacancy(driver)
+
+    # The browser closes and displays the id of vacancies with a failed application
+    finally:
+        driver.quit()
+        print(no_response_vacancy_id)
+
+
+if __name__ == '__main__':
+    main()
